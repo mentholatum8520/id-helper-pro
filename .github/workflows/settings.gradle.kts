3d86b51b87f6c14ef7d483888fac20fdae459747
mentholatum8520 <90280595+mentholatum8520@users.noreply.github.com>
2026-01-31 03:56:45 +0800

    Add settings.gradle.kts for project configuration

---
+++ .github/workflows/settings.gradle.kts
@@ -0,0 +1,17 @@
+kotlin
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "id-helper-pro"
+include(":app")
